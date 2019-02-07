90b5affabf535eabc7a1a7f18a99e0323983d2fa
creatingashes <40171872+creatingashes@users.noreply.github.com>
2019-02-07 18:52:49 -0500

    Add Tangent to the Graph

--- Assignment 1: Python Refresher.py
+++ Assignment 1: Python Refresher.py
@@ -12,15 +12,24 @@ x=np.linspace(0, 2*np.pi) #np.linspace(start, stop, number within the range)
 # Calculate sine and cosine values
 y=np.sin(x)
 z=np.cos(x)
+t=np.tan(x)
 
 # Plot sine and cosine graphs on same set of axes
+# Plot tangent on the axes - Ishani
 plt.plot(x, y, color="red", label="sine")
 
 plt.plot(x, z, color="blue", label="cosine")
 
+plt.plot(x, t, color="yellow", label="tangent")
+
 # Graph title and legend
-plt.title('Sine and Cosine Graph')
+#plt.title('Sine and Cosine Graph')
+# change title to include tangent
+plt.title('Sine, Cosine, Tangent Graph')
 plt.legend()
+
+# make sure y range is from [-1, 1] - Ishani
+plt.ylim(-1, 1)
 
 # x- and y- axis labels
 plt.xlabel('Phase (radians)')
@@ -32,7 +41,11 @@ plt.axhline(y=0, color='k')
 # Add grid lines
 plt.grid(color='black', linestyle='-', linewidth=0.2)
 
+
+
 # Label the x-axis tick locations 
 plt.xticks((0,np.pi/2,np.pi,np.pi*3/2,np.pi*2),('0','π/2','π','3π/2','2π'))
 
+# Saving the graph locally - Ishani
+plt.savefig('Sine, Cosine, Tangent')
 plt.show()
